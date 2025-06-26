95e965d75479571ddd1b2e9cb353bdfae80926a3
Omar Gamal <omarthefirst6206@gmail.com>
2025-06-26 12:48:08 +0300

    update app label to "Contacts" in AndroidManifest.xml and Info.plist; add CustomFloatingActionButton with icon; remove unused colors.dart file; add contact_image.png

--- lib/features/home/view/home_screen.dart
+++ lib/features/home/view/home_screen.dart
@@ -1,14 +1,38 @@
+import 'package:contact_app/features/home/floating_action_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-  static const routename ="home";
+  static const routename = "home";
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor:Color(0xff29384D),
-
+    return Scaffold(
+      backgroundColor: Color(0xff29384D),
+      floatingActionButton: CustomFloatingActionButton(),
+      body: Column(
+        children: [
+          SizedBox(height: 150.h),
+          Image.asset(
+            'assets/images/contact_image.png',
+            fit: BoxFit.cover,
+            //BackgroundColor: Color(0xff29384D),
+            width: 368.w,
+            height: 368.h,
+          ),
+          Center(
+            child: Text(
+              'There is No Contacts Added Here',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.sp,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
